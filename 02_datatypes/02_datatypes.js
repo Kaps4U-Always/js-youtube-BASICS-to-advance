ff9555bb6cc47936fa10ea28402bebfa3d566c4f
Kaps4U-Always <kapil.belsare101@gmail.com>
2025-08-12 14:52:49 +0000

    datatypes

---
+++ 02_datatypes/02_datatypes.js
@@ -0,0 +1,19 @@
+let Name ="Kapil"
+let age=35
+let IsIndianCItizen=true
+
+console.log(Name);
+console.log(age);
+console.log(IsIndianCItizen);
+
+//number => 2 to power 53 or 52
+//bigint
+//string => string
+//boolean => true/false
+//null => standalone value type of - object
+//undefined => undefined
+//symbol => unique type
+
+console.table([Name,age,IsIndianCItizen])
+
+
