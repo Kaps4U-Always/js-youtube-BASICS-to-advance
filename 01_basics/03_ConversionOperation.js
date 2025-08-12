70e1e706ea5ca7c40798f46287a9679d2932c602
Kaps4U-Always <kapil.belsare101@gmail.com>
2025-08-12 16:00:29 +0000

    conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,11 @@
+// let score=99;
+// let score="99"
+// let score="99Abs"
+let score=null
+
+let ScoreIsNumber=Number(score)
+
+console.log(typeof score)
+console.log(typeof ScoreIsNumber)
+
+console.log(ScoreIsNumber)
